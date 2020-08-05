a2b2db90c21c5f971cc3b949148d01a13aa0128e
Dominic Lee <dominiclee0512@gmail.com>
2020-08-05 23:51:11 +0800

    feat: resolve mlab database connect issue.

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,10 @@ const initialize = () => {
   const { NODE_ENV, MONGODB_URI, MONGODB_NAME } = process.env
   const isProduction = NODE_ENV === 'production'
 
-  mongoose.connect(`${MONGODB_URI}/${MONGODB_NAME}?retryWrites=true&w=majority`)
+  mongoose.connect(
+    `${MONGODB_URI}/${MONGODB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
 
   if (!isProduction) {
     mongoose.set('debug', true)
